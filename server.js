820803771535b42d6a352947098bf362e100503f
sagar2inf <csagarkm@gmail.com>
2025-10-18 21:00:16 +0000

    edited the server.js

--- server.js
+++ server.js
@@ -6,23 +6,28 @@ import teamRoutes from "./routes/teamRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-dotenv.config();
 
+dotenv.config();
 const app = express();
 
-app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://teamsync-frontend-h8sa.vercel.app",],
+    origin: [
+        "http://localhost:5173", 
+        "https://teamsync-frontend-h8sa.vercel.app", 
+        "https://teamsync-frontend-h8sa-git-main-sagars-projects-57ba4a6a.vercel.app",
+        "https://teamsync-frontend-h8sa-mjxtwu7xj-sagars-projects-57ba4a6a.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-connectDB();
 const port = process.env.PORT;
+app.use(cookieParser());
 app.use(express.json());
 
+connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
